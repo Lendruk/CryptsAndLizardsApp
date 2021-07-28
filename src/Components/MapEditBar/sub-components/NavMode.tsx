8c34fb02781a641725637b2d3b02d894349656b7
Lendruk <fenixeye3@gmail.com>
2021-07-28 18:46:33 +0100

    Sidebar style changes

--- src/Components/MapEditBar/sub-components/NavMode.tsx
+++ src/Components/MapEditBar/sub-components/NavMode.tsx
@@ -1,11 +1,11 @@
-import { DashboardOutlined, FlagOutlined, SettingOutlined } from '@ant-design/icons';
+import { DashboardOutlined, FlagOutlined, ReadOutlined, SettingOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
 type Route = {
-  url: string;
+  url: string | null;
   displayTxt: string;
   icon?: ReactElement<any,any>;
   subRoutes: Route[];
@@ -25,6 +25,23 @@ const ROUTES: Route[] = [
     subRoutes: []
   },
   {
+    url: null,
+    displayTxt: 'Assets',
+    icon: <ReadOutlined />,
+    subRoutes: [
+      {
+        displayTxt: 'My Assets',
+        url: '/assets/me',
+        subRoutes: []
+      },
+      {
+        displayTxt: 'Browse Assets',
+        url: '/assets',
+        subRoutes: []
+      }
+    ]
+  },
+  {
     url: '/settings',
     displayTxt: 'Settings',
     icon: <SettingOutlined />,
@@ -34,12 +51,23 @@ const ROUTES: Route[] = [
 
 
 export default function NavMode() {
-  const [selectedRoute, setSelectedRoute] = useState('/');
+  const [selectedRoute, setSelectedRoute] = useState('Dashboard');
 
-  const RenderRoute = (route: Route) => (
-    <div className={`Route ${selectedRoute === route.url ? 'selected' : ''}`}>
-      {route.icon}
-      <Link onClick={() => setSelectedRoute(route.url)} to={route.url}>{route.displayTxt}</Link>
+  const RenderRoute = (route: Route, parent?: string) => (
+    <div className={`Route ${route.subRoutes.length === 0 ? 'BorderOnHover': ''} ${parent ? 'SubRoute': ''} ${selectedRoute === route.displayTxt ? 'selected' : ''}`}>
+      <section style={{ width: '100%' }}>
+        {route.icon}
+        {route.url ? (
+          <Link onClick={() => setSelectedRoute(route.displayTxt)} to={route.url}>{route.displayTxt}</Link>
+        ): (
+          <span>{route.displayTxt}</span>
+        )}
+      </section>
+      {route.subRoutes && (
+        <div style={{ width: '100%' }}>
+          {route.subRoutes.map(subRoute => RenderRoute(subRoute, route.displayTxt))}
+        </div>
+      )}
     </div>
   )
 
--- src/Views/MainApp/MainApp.tsx
+++ src/Views/MainApp/MainApp.tsx
@@ -10,11 +10,25 @@ import Register from '../Auth/Register';
 import { ReduxAction, Store } from '../../Redux/reducers';
 import Dashboard from '../Dashboard/Dashboard';
 import Sidebar from '../../Components/MapEditBar/MapEditBar';
+import { Mosaic } from 'react-mosaic-component';
 
 export default function MainApp() {
   const session = useSelector<Store>((state) => state.sessionReducer);
   const inGame = useSelector<Store, ReduxAction<boolean>>((state) => state.inGame);
   console.log(inGame);
+
+  const ELEMENT_MAP : { [key: string]: JSX.Element } = {
+    'content':
+    <div className="MainAppOutput">
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/game" component={Game} />
+        <Route exact path="/mapEditor" component={MapEditor} />
+        <Route exact path="/campaigns" component={Campaigns} />
+      </Switch>
+    </div>,
+    'sidebar': <Sidebar />
+  }
   return (
     <>
       <BrowserRouter>
@@ -22,15 +36,17 @@ export default function MainApp() {
           <div style={{ width: '100%' }}className="DarkTheme">
           {session ? (
             <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%' }}>
-              <Sidebar />
-              <div className="MainAppOutput">
-                <Switch>
-                  <Route exact path="/" component={Dashboard} />
-                  <Route exact path="/game" component={Game} />
-                  <Route exact path="/mapEditor" component={MapEditor} />
-                  <Route exact path="/campaigns" component={Campaigns} />
-                </Switch>
-              </div>
+                <Mosaic<string>
+                    renderTile={(id) => ELEMENT_MAP[id]}
+                    initialValue={{
+                     direction: 'row',
+                      first: 'sidebar',
+                       second: 'content',
+                       splitPercentage: 10,
+                    }}
+                    resize={{ minimumPaneSizePercentage: 3 }}
+                />
+
             </div>
           ) : ( 
             <div className="MainAppOutput">
